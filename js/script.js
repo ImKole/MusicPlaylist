8e3d48d41f207f87b2b71b25559dc1f44fa2b8f9
joeym <jemitch@pointpark.edu>
2025-02-02 16:25:06 -0500

    Adding mock Json interaction to Saved Playlist page

--- js/script.js
+++ js/script.js
@@ -46,3 +46,22 @@ document.addEventListener('DOMContentLoaded', function () {
     // Initial check in case the checkbox is saved as checked
     updateMode();
 });
+
+
+//Mock Json request for the database to gather and input saved playlists onto the page
+function displaySavedPlaylists(playlists) {
+    const container = document.querySelector('.container');
+    playlists.forEach(playlist => {
+        const playlistElement = document.createElement('div');
+        playlistElement.className = 'playlist-container-main';
+        playlistElement.innerHTML = `
+            <img class="item-image" src="${playlist.image}">
+            <p>${playlist.title}</p>
+            <div class="count-container">
+                <p class="count">${playlist.songCount}</p>
+                <h3>Song Count</h3>
+            </div>
+        `;
+        container.appendChild(playlistElement);
+    });
+}
