71b72e2582d640e4a9d0190110c8abac1b2a08f7
joeym <jemitch@pointpark.edu>
2025-02-02 16:18:02 -0500

    Adding dark mode functionality to Saved Playlist page

--- js/script.js
+++ js/script.js
@@ -25,3 +25,24 @@ document.querySelector('.searchBar').addEventListener('input', function() {
         }
     });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    var checkbox = document.getElementById('darkmode-toggle');
+
+    // Check the checkbox status and update the mode
+    function updateMode() {
+        if (checkbox.checked) {
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+        }
+    }
+
+    // Event listener for changes on the checkbox
+    checkbox.addEventListener('change', function() {
+        updateMode();
+    });
+
+    // Initial check in case the checkbox is saved as checked
+    updateMode();
+});
